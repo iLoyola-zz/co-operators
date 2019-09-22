c1a6dd1fcf6b1c5b005995836d6714498fb3488c
Ivan Loyola <ivan@iloyola.com>
2019-09-22 17:33:17 -0400

    Responsive styling using bootstrap

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -15,33 +15,46 @@ const Questions = (props) => {
       }
     }
     return (
-      <Container>
-        <Row>
-          <Col className="il-coop-container-row-column">
-            <h2 className="il-coop-container-row-column-heading">
-              {questions[questionIndex - 1].QuestionCategory}
-            </h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="il-coop-container-row-column">
-            <p className="il-coop-container-row-column-text">
-              {questions[questionIndex - 1].QuestionText}
-            </p>
-          </Col>
-        </Row>
-        <Row className="il-coop-container-row">
-          <Col className="il-coop-container-row-column">
-            <Rating
-              onClick={(value) => props.handleRating(value)}
-              start={0}
-              stop={10}
-              step={1}
-              fractions={2}
-              initialRating={initialRating()} />
-          </Col>
-        </Row>
-      </Container>
+      <article>
+        <Container className="il-coop-container il-coop-container-2">
+          <Row>
+            <Col xs={12} className="il-coop-container-row-column">
+              <h2 className="il-coop-container-row-column-heading">
+                {questions[questionIndex - 1].QuestionCategory}
+              </h2>
+            </Col>
+          </Row>
+        </Container>
+        <Container className="il-coop-container il-coop-container-3">
+          <Row>
+            <Col xs={12} className="il-coop-container-row-column">
+              <p className="il-coop-container-row-column-text">
+                {questions[questionIndex - 1].QuestionText}
+              </p>
+            </Col>
+          </Row>
+        </Container>
+        <Container className="il-coop-container il-coop-container-4">
+          <Row className="il-coop-container-row">
+            <Col xs={{ span: 6, order: 1 }} sm={{ span: 4, order: 1 }} md={4.5}  className="il-coop-container-row-column il-coop-rating-disagree">
+              <span className="il-coop-rating-disagree-text">Strongly Disagree</span>
+            </Col>
+            <Col xs={{ span: 12, order: 3 }} sm={{ span: 4, order: 2 }} md={3} className="il-coop-container-row-column il-coop-rating-comp">
+              <Rating
+                onClick={(value) => props.handleRating(value)}
+                start={0}
+                stop={5}
+                step={1}
+                fractions={2}
+                initialRating={initialRating()}
+                className="il-coop-rating" />
+            </Col>
+            <Col xs={{ span: 6, order: 2 }} sm={{ span: 4, order: 3 }} md={4.5} className="il-coop-container-row-column il-coop-rating-agree">
+              <span className="il-coop-rating-agree-text">Strongly Agree</span>
+            </Col>
+          </Row>
+        </Container>
+      </article>
     )
 }
 
--- src/pages/SurveyPage.js
+++ src/pages/SurveyPage.js
@@ -47,7 +47,7 @@ class SurveyPage extends React.Component {
   render() {
     return (
       <section className="il-coop-section">
-        <Container className="il-coop-container">
+        <Container className="il-coop-container il-coop-container-1">
           <Row className="il-coop-container-row">
             <Col className="il-coop-container-row-column">
               <Header title={this.state.title} />
@@ -55,15 +55,18 @@ class SurveyPage extends React.Component {
           </Row>
         </Container>
         {this.state.questions.length ? (<Questions questionIndex={this.state.questionIndex} questions={this.state.questions} handleRating={this.handleRating} />) : ('Loading question...')}
-        <Container>
+        <Container className="il-coop-container il-coop-container-5">
           <Row className="il-coop-container-row">
-            <Col xs={3} md={2} className="il-coop-container-row-column il-coop-col-previous-button">
+            <Col xs={{ span:6, order:1 }} md={{ span:2, order:1 }} className="il-coop-container-row-column il-coop-col-previous-button">
               <Button onClick={this.handlePreviousQuestion} disabled={this.state.questionIndex === 1} variant="primary" className="il-coop-button" size="lg" block>Previous</Button>
             </Col>
-            <Col xs={12} md={8} className="il-coop-container-row-column">
+            <Col xs={{ span:12, order:3 }} md={{ span:8, order:2 }} className="il-coop-container-row-column">
               <ProgressBar now={(100 / this.state.questions.length) * this.state.questionIndex} />
+              <div className="il-coop-footer">
+                <p>{this.state.questionIndex} of {this.state.questions.length}</p>
+              </div>
             </Col>
-            <Col xs={3} md={2} className="il-coop-container-row-column il-coop-col-next-button">
+            <Col xs={{ span:6, order:2 }} md={{ span:2, order:3 }} className="il-coop-container-row-column il-coop-col-next-button">
               <Button onClick={this.handleNextQuestion} disabled={this.state.questionIndex === this.state.questions.length} variant="primary" className="il-coop-button" size="lg" block>Next</Button>
             </Col>
           </Row>
